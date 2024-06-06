7093bef81f0c276e4776292dcd3ea46699fa55ad
shubh81096 <shubhamsshedge@com>
2024-06-06 12:29:17 +0530

    shubham commited

--- src/test/java/com/base/Base.java
+++ src/test/java/com/base/Base.java
@@ -2,8 +2,11 @@ package com.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -11,43 +14,54 @@ import com.utility.Utility;
 
 public class Base {
 
-	public static WebDriver driver;
+    public static WebDriver driver;
 
-	public String browser = Utility.getPropertyDirectly("browser");
-	public String url = Utility.getPropertyDirectly("url");
+    public String browser = Utility.getPropertyDirectly("browser");
+    public String url = Utility.getPropertyDirectly("url");
+    public boolean headless = Boolean.parseBoolean(Utility.getPropertyDirectly("headless"));
 
-	@BeforeClass
-	public void browserSetup() throws Exception {
+    @BeforeClass
+    public void browserSetup() throws Exception {
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            if (headless) {
+                options.addArguments("--headless");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+            }
+            driver = new ChromeDriver(options);
 
-		if (browser.equalsIgnoreCase("chrome")) {
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            if (headless) {
+                options.addArguments("--headless");
+            }
+            driver = new FirefoxDriver(options);
 
-			driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            if (headless) {
+                options.addArguments("headless");
+                options.addArguments("disable-gpu");
+            }
+            driver = new EdgeDriver(options);
 
-		} else if (browser.equalsIgnoreCase("firefox")) {
+        } else {
+            throw new Exception("Invalid Browser Value");
+        }
 
-			driver = new FirefoxDriver();
+        driver.get(url);
+    }
 
-		} else if (browser.equalsIgnoreCase("edge")) {
+    public static WebDriver getDriver() {
+        return driver;
+    }
 
-			driver = new EdgeDriver();
-
-		} else {
-
-			throw new Exception("Invalid Browser Value");
-
-		}
-
-	}
-
-	public static WebDriver getDriver() {
-		return driver;
-	}
-
-	@AfterClass
-	public void tearDown() throws InterruptedException {
-		Thread.sleep(3000);
-		driver.quit();
-
-	}
-
+    @AfterClass
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
